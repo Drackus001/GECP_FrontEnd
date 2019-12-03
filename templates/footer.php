79c0c88faa20cd37c80a353b4f44311d4b196276
root <root@localhost.localdomain>
2019-12-04 00:40:47 +0530

    some formatting code

--- templates/footer.php
+++ templates/footer.php
@@ -1,111 +1,119 @@
   <!-- Footer -->
   <footer class="footer" id="footer">
-    <div class="container">
-    	<div class="row">
-	      
-	      <div class="col-md-4 col-sm-12 footer-widget">
-	        <h5 class="widget-title">Recent Posts</h5>
-	        
-	      </div>
-	      <!--/ End Recent Posts-->
+      <div class="container">
+          <div class="row">
+
+              <div class="col-md-4 col-sm-12 footer-widget">
+                  <h5 class="widget-title">Recent Posts</h5>
+
+              </div>
+              <!--/ End Recent Posts-->
 
 
-	      <div class="col-md-4 col-sm-12 footer-widget">
-	        <h5 class="widget-title"> Photos</h5>
+              <div class="col-md-4 col-sm-12 footer-widget">
+                  <h5 class="widget-title"> Photos</h5>
 
-	        <div class="img-gallery">
-	          <div class="img-container">
-	            <a class="thumb-holder" data-rel="prettyPhoto" href="img/gallery/1.jpg">
-	              <img src="img/gallery/1.jpg" alt="IMG">
-	            </a>
-	            <a class="thumb-holder" data-rel="prettyPhoto" href="img/gallery/2.jpg">
-	              <img src="img/gallery/2.jpg" alt="IMG">
-	            </a>
-	            <a class="thumb-holder" data-rel="prettyPhoto" href="img/gallery/3.jpg">
-	              <img src="img/gallery/3.jpg" alt="IMG">
-	            </a>
-	            <a class="thumb-holder" data-rel="prettyPhoto" href="img/gallery/4.jpg">
-	              <img src="img/gallery/4.jpg" alt="IMG">
-	            </a>
-	            <a class="thumb-holder" data-rel="prettyPhoto" href="img/gallery/5.jpg">
-	              <img src="img/gallery/5.jpg" alt="IMG">
-	            </a>
-	            <a class="thumb-holder" data-rel="prettyPhoto" href="img/gallery/6.jpg">
-	              <img src="img/gallery/6.jpg" alt="IMG">
-	            </a>
-	            <a class="thumb-holder" data-rel="prettyPhoto" href="img/gallery/6.jpg">
-	              <img src="img/gallery/7.jpg" alt="IMG">
-	            </a>
-	            <a class="thumb-holder" data-rel="prettyPhoto" href="img/gallery/6.jpg">
-	              <img src="img/gallery/8.jpg" alt="IMG">
-	            </a>
-	            <a class="thumb-holder" data-rel="prettyPhoto" href="img/gallery/6.jpg">
-	              <img src="img/gallery/9.jpg" alt="IMG">
-	            </a>
-	          </div>
-	        </div>
-	      </div>
-	      <!--/ end flickr -->
+                  <div class="img-gallery">
+                      <div class="img-container">
+                          <a class="thumb-holder" data-rel="prettyPhoto" href="img/gallery/1.jpg">
+                              <img src="img/gallery/1.jpg" alt="IMG">
+                          </a>
+                          <a class="thumb-holder" data-rel="prettyPhoto" href="img/gallery/2.jpg">
+                              <img src="img/gallery/2.jpg" alt="IMG">
+                          </a>
+                          <a class="thumb-holder" data-rel="prettyPhoto" href="img/gallery/3.jpg">
+                              <img src="img/gallery/3.jpg" alt="IMG">
+                          </a>
+                          <a class="thumb-holder" data-rel="prettyPhoto" href="img/gallery/4.jpg">
+                              <img src="img/gallery/4.jpg" alt="IMG">
+                          </a>
+                          <a class="thumb-holder" data-rel="prettyPhoto" href="img/gallery/5.jpg">
+                              <img src="img/gallery/5.jpg" alt="IMG">
+                          </a>
+                          <a class="thumb-holder" data-rel="prettyPhoto" href="img/gallery/6.jpg">
+                              <img src="img/gallery/6.jpg" alt="IMG">
+                          </a>
+                          <a class="thumb-holder" data-rel="prettyPhoto" href="img/gallery/6.jpg">
+                              <img src="img/gallery/7.jpg" alt="IMG">
+                          </a>
+                          <a class="thumb-holder" data-rel="prettyPhoto" href="img/gallery/6.jpg">
+                              <img src="img/gallery/8.jpg" alt="IMG">
+                          </a>
+                          <a class="thumb-holder" data-rel="prettyPhoto" href="img/gallery/6.jpg">
+                              <img src="img/gallery/9.jpg" alt="IMG">
+                          </a>
+                      </div>
+                  </div>
+              </div>
+              <!--/ end flickr -->
 
-	      <div class="col-md-3 col-sm-12 footer-widget footer-about-us">
-	        <h5 class="widget-title"> Contact</h5><br>
-	        <div class="row widget-title">
-	           <div class="col-md-12" style="text-align: left;">
-	            <p >Address: <span style="color: #fec503;">At & Post. Katpur,Patan</span></p>   
-	          </div>
-	          <div class="col-md-12" style="text-align: left;">
-	            <p >Email: <a href="mailto:principalgecpatan@gmail.com">principalgecpatan@gmail.com</a></p>   
-	          </div>
-	          <div class="col-md-12" style="text-align: left;">
-	            <p>Phone No: <span style="color: #fec503;">+2766291560</span></p>
-	            
-	          </div>
-	        </div>
-	      </div>
-	      <!--/ end about us -->
+              <div class="col-md-3 col-sm-12 footer-widget footer-about-us">
+                  <h5 class="widget-title"> Contact</h5><br>
+                  <div class="row widget-title">
+                      <div class="col-md-12" style="text-align: left;">
+                          <p>Address: <span style="color: #fec503;">At & Post. Katpur,Patan</span></p>
+                      </div>
+                      <div class="col-md-12" style="text-align: left;">
+                          <p>Email: <a href="mailto:principalgecpatan@gmail.com">principalgecpatan@gmail.com</a></p>
+                      </div>
+                      <div class="col-md-12" style="text-align: left;">
+                          <p>Phone No: <span style="color: #fec503;">+2766291560</span></p>
 
-	    </div><!-- Row end -->
-	    <hr>
-      <div class="row align-items-center">
-        <div class="col-md-4">
-          <span class="copyright">Copyright  &copy; . All Rights Reserved</span>
-        </div>
-        <div class="col-md-4">
-          <ul class="list-inline social-buttons">
-            <li class="list-inline-item">
-              <a href="#">
-                <i class="fab fa-twitter"></i>
-              </a>
-            </li>
-            <li class="list-inline-item">
-              <a href="#">
-                <i class="fab fa-facebook-f"></i>
-              </a>
-            </li>
-            <li class="list-inline-item">
-              <a href="#">
-                <i class="fab fa-linkedin-in"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div class="col-md-4">
-       <span class="copyright">Designed and Developed by<br>
-       	<a href="#">Students of GEC,Patan</a></span>
-        </div>
+                      </div>
+                  </div>
+              </div>
+              <!--/ end about us -->
+
+          </div><!-- Row end -->
+          <hr>
+          <div class="row align-items-center">
+              <div class="col-md-4">
+                  <span class="copyright">Copyright &copy; . All Rights Reserved</span>
+              </div>
+              <div class="col-md-4">
+                  <ul class="list-inline social-buttons">
+                      <li class="list-inline-item">
+                          <a href="#">
+                              <i class="fab fa-twitter"></i>
+                          </a>
+                      </li>
+                      <li class="list-inline-item">
+                          <a href="#">
+                              <i class="fab fa-facebook-f"></i>
+                          </a>
+                      </li>
+                      <li class="list-inline-item">
+                          <a href="#">
+                              <i class="fab fa-linkedin-in"></i>
+                          </a>
+                      </li>
+                  </ul>
+              </div>
+              <div class="col-md-4">
+                  <span class="copyright">Designed and Developed by<br>
+                      <a href="#">Students of GEC,Patan</a></span>
+              </div>
+          </div>
+          <div id="back-to-top" data-spy="affix" data-offset-top="10" class="back-to-top affix position-fixed">
+              <a class="btn btn-primary" href="#page-top"><i class="fa fa-angle-double-up"></i></a>
+          </div>
       </div>
-      <div id="back-to-top" data-spy="affix" data-offset-top="10" class="back-to-top affix position-fixed">
-	      <a class="btn btn-primary" href="#page-top"><i class="fa fa-angle-double-up"></i></a>
-	    </div>
-	  </div>
-    </div>
+      </div>
   </footer>
-  
+
 
   <!-- Bootstrap core JavaScript -->
   <script src="vendor/jquery/jquery.min.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-</body>
 
-</html>
+  <!-- AOS script -->
+  <script type="text/javascript">
+      AOS.init({
+          duration: 1200;
+      })
+  </script>
+
+  </body>
+
+  </html>
