b8faaa03e9c209d90976879d2e4592012fc8d1a3
root <root@localhost.localdomain>
2019-12-03 23:43:58 +0530

    converted in php folder structure except faculty.php

---
+++ templates/nav.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Home :: Government Engineering College, Patan</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">	
+
+  <!-- Custom fonts for this template -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+  <!-- Custom styles for this template -->
+  <link href="css/globalStyle.css" rel="stylesheet">
+  <link href="css/facStyle.css" rel="stylesheet">
+  
+  <link href="css/style.css" rel="stylesheet">
+  <style type="text/css">
+  	#mainNav{
+    padding-top: 0;
+    padding-bottom: 0;
+    background-color: #484848;
+  }
+  </style>
+
+</head>
+<body id="page-top">
+
+  
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+    <div class="container-fluid">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top"><img class="img-fluid float-left" src="img/logos/gec.png" alt="logo"></a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav text-uppercase ml-auto">
+        <li class="active nav-link js-scroll-trigger"><a href="#">Home</a></li>
+
+        <li class="dropdown">
+          <a class="dropdown-toggle nav-link js-scroll-trigger" data-toggle="dropdown" href="#">About
+          <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li class="dropdown-item"><a href="#about">About College</a></li> <li class="divider"></li> 
+            <li class="dropdown-item"><a href="#">College Campus</a></li> <li class="divider"></li> 
+            <li class="dropdown-item"><a href="tpo.php">Placement Cell</a></li> <li class="divider"></li> 
+            <li class="dropdown-item"><a href="womenCell.php">Women Cell</a></li> <li class="divider"></li> 
+            <li class="dropdown-item"><a href="#">Grievance Cell</a></li> <li class="divider"></li> 
+            <li class="dropdown-item"><a href="ARagging.php">Anti Ragging</a></li> <li class="divider"></li> 
+            <li class="dropdown-item"><a href="ssip.php">Student Startup & Innovation</a></li> <li class="divider"></li> 
+            <li class="dropdown-item"><a href="rti.php">RTI</a></li> <li class="divider"></li> 
+            <li class="dropdown-item"><a href="newsletter.php">Newsletter</a></li>
+          </ul>
+        </li>
+
+        <li class="dropdown">
+          <a class="dropdown-toggle nav-link js-scroll-trigger" data-toggle="dropdown" href="#">Academic<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li class="dropdown-item"><a href="#">Acadmemic Calendar</a></li> <li class="divider"></li> 
+            <li class="dropdown-item"><a href="#">Programs</a></li> <li class="divider"></li> 
+            <li class="dropdown-item"><a href="#">Labs</a></li> <li class="divider"></li> 
+            <li class="dropdown-item"><a href="#">Workshop</a> <li class="divider"></li> 
+          </ul>
+        </li>
+        <li><a class="nav-link js-scroll-trigger" href="faculty.php">People</a></li>
+        <li><a class="nav-link js-scroll-trigger" href="#footer">Gallery</a></li>
+        <li><a class="nav-link js-scroll-trigger" href="downloads.php">Downloads</a></li>
+        <li><a class="nav-link js-scroll-trigger" href="#footer">Contact Us</a></li>
+        </ul>
+
+      </div>
+      <!--/.nav-collapse -->
+    </div>
+    <!--/.container-fluid -->
+  </nav>
