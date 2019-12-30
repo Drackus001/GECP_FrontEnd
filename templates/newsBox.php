ccab3c0a1d0bde6e7c9d76c24b6fbd0127dbef52
SATYAM679 <679agarsatya@gmail.com>
2019-12-30 15:06:47 +0530

    dynamic news is working but uploads/ is not workin

--- templates/newsBox.php
+++ templates/newsBox.php
@@ -1,32 +1,26 @@
 <!-- News Box Start -->
 <div class="col-xs-6 col-lg-4 news-box">
-              <div class=" news">
+              <div class="news">
                 <h4 class="text-center">Latest News</h4>
                 <div class="news-list">
-                  <ul>
-                    <li>dhdf</li>
-                    <li>sgdsdgd</li>
-                    <li>sdfdsfdnfjdfn</li>
-                    <li>dkfknsdfndkf</li>
-                    <li>dmskndfnsmdf</li>
-                    <li>dfndn,dn</li>
-                    <li>dhdf</li>
-                    <li>sgdsdgd</li>
-                    <li>sdfdsfdnfjdfn</li>
-                    <li>dkfknsdfndkf</li>
-                    <li>dmskndfnsmdf</li>
-                    <li>dfndn,dn</li><li>dhdf</li>
-                    <li>sgdsdgd</li>
-                    <li>sdfdsfdnfjdfn</li>
-                    <li>dkfknsdfndkf</li>
-                    <li>dmskndfnsmdf</li>
-                    <li>dfndn,dn</li><li>dhdf</li>
-                    <li>sgdsdgd</li>
-                    <li>sdfdsfdnfjdfn</li>
-                    <li>dkfknsdfndkf</li>
-                    <li>dmskndfnsmdf</li>
-                    <li>dfndn,dn</li>
-                  </ul>
+                <?php
+                            $sql = "SELECT * FROM news;";
+                            $result = mysqli_query($conn, $sql);
+                            $resultCheck = mysqli_num_rows($result);
+                            if ($resultCheck > 0) {
+                            
+                              // echo '<ul>';
+                            
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                  echo '<p><strong>&nbsp;'.$row['date_of_creation'].'</strong><br>>>&nbsp;<a style="color:black"; href="'.$row['path'].'">'.$row['name'].'</a></p>';      
+                                }
+                            // echo '</ul>';
+                            } 
+                            else {
+                                echo 'SQL ERROR';
+                            }
+                            ?>
+                  
                 </div>
               </div>
           </div>
