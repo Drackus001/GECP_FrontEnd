2567a5b101bdfd5606e24db0eba6c4be61a97353
SATYAM679 <679agarsatya@gmail.com>
2019-12-30 14:38:16 +0530

    latest news right side dummy data is fixed

--- index.php
+++ index.php
@@ -30,17 +30,40 @@ include_once './templates/slider.php';
  ?>
   <!--/ Slider end -->
 
-<a href="profile.php">detail profile </a><br>
+<!-- <a href="profile.php">detail profile </a><br> -->
 
 <!--News Start-->
   <section class="page-section" id="temp">
     <div class="container-fluid">
       <div class="row">
           <div class="col-xs-6 col-lg-8 text-center">
-              <h2 class="section-heading text-uppercase">About</h2>
-              <h3 class="section-subheading text-muted">Government Engineering College,at. Katpur, Patan</h3>
-              <p class="text-left">Government Engineering College, Patan was established in April 2004 with three branches Computer Engineering, Electronics and Communication Engineering and Mechanical Engineering each with intake of 60 totaling to 180. The institute was initially functioning in the premises of the K. D. Polytechnic, Patan temporarily. It was shifted to its own newly built up green premise in August 2008 at Katpur village on Chanasma- Patan road 8 kms before Patan. Two more branches of Electrical Engineering and Civil Engineering each with intake of 60 were introduced from June- 2009 and the intake of all three existing branches were increased to 120. Currently, institute functions with total intake of 480 in five different branches. </p>
-              <p class="text-left">Government Engineering College, Patan was established in April 2004 with three branches Computer Engineering, Electronics and Communication Engineering and Mechanical Engineering each with intake of 60 totaling to 180. The institute was initially functioning in the premises of the K. D. Polytechnic, Patan temporarily. It was shifted to its own newly built up green premise in August 2008 at Katpur village on Chanasma- Patan road 8 kms before Patan. Two more branches of Electrical Engineering and Civil Engineering each with intake of 60 were introduced from June- 2009 and the intake of all three existing branches were increased to 120. Currently, institute functions with total intake of 480 in five different branches. </p>
+                          <?php
+                            $sql = "SELECT vision,mission FROM college_info;";
+                            $result = mysqli_query($conn, $sql);
+                            $resultCheck = mysqli_num_rows($result);
+                            if ($resultCheck > 0) {
+                            
+                            
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                  echo '<h2 class="section-heading text-uppercase">Mandate</h2><hr>
+                                  <p class="text-left">To impart the technical education as per the state and national policy which satisfy the current needs of the society, to interact with the industry and research organizations for the integrated growth, to make the liaison with statutory and professional organizations and to provide the training and research opportunities to the student and faculties in best possible manner. </p><br><br>';
+                                  echo '<h2 class="section-heading text-uppercase">Vision</h2><hr>
+                                  <p class="text-left">'.$row['vision'].' </p><br><br>';
+
+                                  echo '<h2 class="section-heading text-uppercase">Mission</h2><hr>
+                                  <ul>
+                                  <li class="text-left">To deliver technical programs and services to cater the current needs of society and industry.</li>
+                                  <li class="text-left">Helping industries in solving challenges by means of providing best technical human resources.</li>
+                                  <li class="text-left">To contribute in sustainable growth of society.</li>
+                                  </ul>
+                                  ';
+                                  
+                                }
+                                
+                            } else {
+                                echo 'SQL ERROR';
+                            }
+                            ?>
             </div>
 
           <!-- News Box Start -->
